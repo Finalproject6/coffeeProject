bf5e068d079b85c0d7622663d8c86e3df21e91b6
ahmadhamzh <ahmadhamzh@ymail.com>
2021-07-12 19:32:00 +0300

    css changes

--- js/cart.js
+++ js/cart.js
@@ -25,10 +25,10 @@ let totalSum = 0;
 let divPrint = document.getElementById('print');
 let btn1;
 for (let i = 0; i < btnArr.length; i++) {
-    
+
     btn1 = document.getElementById(btnArr[i]);
     btn1.addEventListener('click', handelClick);
-    
+
 }
 
 let sum;
@@ -36,10 +36,11 @@ function handelClick(event) {
     for (let i = 0; i < btnArr.length; i++) {
         if (btnArr[i] == event.target.id) {
             if (idArr.includes(btnArr[i])) {
-                alert('you already take')
+                swal("you already have that Item in the Cart!!");
+
                 return
             }
-            
+
             idArr.push(btnArr[i]);
             let div1 = document.createElement('div');
             divPrint.appendChild(div1);
@@ -48,13 +49,13 @@ function handelClick(event) {
             img.setAttribute("src", globArr[i].path);
             let ProductName = document.createElement('h4');
             div1.appendChild(ProductName);
-            ProductName.textContent = globArr[i].name + ' ' + globArr[i].price + '.00 $ / packge';
+            ProductName.textContent = globArr[i].name + '' + globArr[i].price + '.00 $ / packge';
             let inbutDecrease = document.createElement('button');
             div1.appendChild(inbutDecrease);
-            inbutDecrease.setAttribute('id','decrease')
-            let inbutIncrease = document.createElement('button');            
+            inbutDecrease.setAttribute('id', 'decrease')
+            let inbutIncrease = document.createElement('button');
             div1.appendChild(inbutIncrease);
-            inbutIncrease.setAttribute('id','increase')
+            inbutIncrease.setAttribute('id', 'increase')
             let allPrice = document.createElement('p');
             div1.appendChild(allPrice);
             inbutDecrease.addEventListener('click', handelDecrease);
@@ -62,10 +63,10 @@ function handelClick(event) {
             handelIncrease();
             let deletButton = document.createElement('button');
             div1.appendChild(deletButton);
-            deletButton.setAttribute('id','delete')
+            deletButton.setAttribute('id', 'delete')
             deletButton.addEventListener('click', deletClick);
 
-            function deletClick(){
+            function deletClick() {
 
                 divPrint.removeChild(div1);
                 let newIdArr = idArr;
@@ -73,60 +74,146 @@ function handelClick(event) {
                 for (let j = 0; j < newIdArr.length; j++) {
                     if (btnArr[i] != newIdArr[j]) {
                         idArr.push(newIdArr[j])
-                        globArr[i].price=0
-                        
-                        totalSum1 ()
+                        globArr[i].quant = 0
+
+                        totalSum1()
                     }
-                    
+                    console.log(idArr);
+                    console.log(idArr.value);
+
                 }
-                
 
+
+                totalSum1()
             }
-            totalSum1 ()
 
-            
+
             function handelIncrease() {
                 globArr[i].quant++
                 sum = globArr[i].quant * globArr[i].price
                 console.log(sum);
                 allPrice.textContent = sum + ' $';
-                totalSum1 ()
-                
+                totalSum1()
+
             }
             // handelDecrease();
             function handelDecrease() {
-                if(globArr[i].quant > 1){
+                if (globArr[i].quant > 1) {
                     globArr[i].quant--;
                     let sum = globArr[i].quant * globArr[i].price
                     console.log(sum);
                     allPrice.textContent = sum + ' $';
-                    totalSum1 ()
-                    
-                    
-                
+                    totalSum1()
+
+
+
                 }
             }
-            
+
         }
     }
-    
+
 }
+
 
 let totalPrice = document.getElementById('totalPrice');
 let totalParagraph = document.createElement('p');
-totalPrice.appendChild(totalParagraph);
+let submit = document.createElement('button');
+// let cancel
 
-function totalSum1 (){
+
+
+
+
+
+function totalSum1() {
+    console.log(idArr.length);
     let finalPrice = 0;
-    for (let i = 0; i < globArr.length; i++) {
-        finalPrice += (globArr[i].price * globArr[i].quant)
+    if (idArr.length === 0) {
+        console.log(idArr.length);
+        totalParagraph.textContent = 'Total = 0.00 $';
+        
+    }else {
+
+        
+        
+        for (let i = 0; i < globArr.length; i++) {
+            finalPrice += (globArr[i].price * globArr[i].quant)
+        }
+        totalPrice.appendChild(totalParagraph);
+        totalPrice.appendChild(submit);
+        console.log(finalPrice);
+        submit.textContent = 'Place you\'r Order'
+        totalParagraph.textContent = 'Total = ' + finalPrice + ' $';
+        console.log(idArr);
+        console.log(btnArr);
+        console.log(idArr.value);
+        
     }
-    console.log(finalPrice);
-    totalParagraph.textContent = 'Total = '+finalPrice;
-    
 
     return finalPrice
-    };
+};
+
+submit.addEventListener('click',placeOrder);
+
+function placeOrder (){
+    custInfo ()
+    if (idArr.length === 0 ) {
+        swal("your Cart is empty!!");
+
+       } else if (nameOfCust.value == false){
+    
+        swal("your Name is empty!!");
+
+       } else if (phoneNum.value == false){ 
+
+        swal("your Phone Number is empty!!");
+
+       } else if (adress.value == false){  
+
+        swal("your Adress is empty!!");         
+        
+    } else {
+
+        swal({
+            title: "Are you sure?",
+            text: "Name : "+nameOfCust.value+"\nPhone Number : "+phoneNum.value+"\nAdress : "+adress.value ,           
+            icon: "info",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal("your order is on prep we will contact you soon", {
+                icon: "success",
+              });
+            } else {
+              swal("your order been cancel",{
+                  icon: "error",
+                })
+            };
+          });
+          totalPrice.removeChild(submit);
+          
+        //   inbutDecrease.removeEventListener('click', handelDecrease);
+        //   inbutIncrease.removeEventListener('click', handelIncrease);
+
+        // alert('you\'r order is on prep we will contact you soon');
+    }
+    
+};
+
+// swal("Hello world!");
+
+let nameOfCust = document.getElementById('name');
+let phoneNum = document.getElementById('phone');
+let adress = document.getElementById('adress');
+function custInfo (){
+    console.log(nameOfCust.value);
+    console.log(phoneNum.value);
+    console.log(adress.value);
+}
+
 
 
 
